da35c57f5a6819403b61044eac284388f1f29c50
gretchenmarie <gretchenmward@gmail.com>
2018-07-13 14:02:00 -0500

    added files

---
+++ main.js
@@ -0,0 +1,14 @@
+//const cookies = ["Oatmeal Raisin", "Chocolate Chip", "Sugar", "Peanut Butter", "Snickerdoodle", "Ginger"]
+//let y = 1
+
+//for (let y = 1; y < cookies.length; y++) {
+ //   const currentCookie = cookies[y]
+ //   console.log(`Mmmmmmm... that's a good ${currentCookie} cookie`)
+//}
+
+const conjunction = function (firstWord, secondWord) {
+    const conjoinedWord = (`${firstWord} ${secondWord}`);
+}
+
+conjunction("Master", "Card")
+console.log(conjoinedWord)
